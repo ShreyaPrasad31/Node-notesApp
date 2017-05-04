6af2e9716d75f2e74b481321f30a4e81de139e55
ShreyaPrasad31 <s9prasad@edu.uwaterloo.ca>
2017-05-04 07:28:38 +0530

    Added Yargs param req

---
+++ appFinal.js
@@ -0,0 +1,68 @@
+const fs = require("fs");
+const os = require("os");
+const yargs = require("yargs");
+const notes = require("./notes");
+
+const _ = require("lodash"); //common name for loadash modules
+const argv = yargs//require title and body of the note 
+.command("add", "Add a new note",{
+	title: {
+		describe: 'Title of note',
+		demand: true,
+		alias: "t"
+	},
+	body: {
+		describe: 'Body of Note',
+		demand: true,
+		alias: "b"
+	}
+})
+
+		
+
+
+.help()
+.argv
+
+var command = process.argv[2];
+if (command == 'add') {
+	var note = notes.addNote(argv.title,argv.body);
+	if (note == undefined){
+		console.log("Note title taken");
+	}else {
+		console.log("added note!");
+		console.log("---");
+		console.log(`Title: ${note.title}`);
+		console.log(`Body: ${note.body}`);
+	};
+} else if (command =='list'){
+	var allNotes = notes.getAll();
+	console.log(`Printing ${allNotes.length} note(s).`);
+	allNotes.forEach(() => {
+		notes.logNote(note);
+
+	});
+} else if (command == 'remove'){
+	var noteRemoved = notes.remove(argv.title);
+	var message = noteRemoved ? "The note was removed" : "Note not found";
+	console.log(message);
+} else if (command == 'read'){
+	var note = notes.getNote(argv.title);
+	if (note){
+		console.log("Note found");
+		console.log("---");
+		console.log(`Title: ${note.title}`);
+		console.log(`Body: ${note.body}`);
+
+	}else{
+		console.log("Note not found");
+		
+	};
+	
+}
+
+else {
+	console.log("Command not recognised");
+};
+
+
